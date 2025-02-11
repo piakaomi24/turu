73dd59898eab6a4c2ea9817a37d92db02c94142d
piakaomiyui24 <piakaomiyui24@gmail.com>
2025-02-11 20:07:55 +0700

    Update

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -4,6 +4,7 @@ import { Link } from "@heroui/link";
 import { button as buttonStyles } from "@heroui/theme";
 import { Card, CardBody, CardFooter, Image } from "@heroui/react";
 import { Breadcrumbs, BreadcrumbItem } from "@heroui/react";
+import {Button} from "@heroui/react";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
@@ -12,44 +13,44 @@ import { GithubIcon } from "@/components/icons";
 export default function Home() {
   const list = [
     {
-      title: "Orange",
+      title: "Supreme Bed",
       img: "/images/nft1.jpg",
-      price: "$5.50",
+      price: "0.1 ETH",
     },
     {
-      title: "Tangerine",
+      title: "Supreme Pillow",
       img: "/images/nft2.jpg",
-      price: "$3.00",
+      price: "0.2 ETH",
     },
     {
-      title: "Raspberry",
+      title: "Cozy Bed",
       img: "/images/nft3.jpg",
-      price: "$10.00",
+      price: "0.3 ETH",
     },
     {
-      title: "Lemon",
+      title: "Starry Pillow",
       img: "/images/nft4.jpg",
-      price: "$5.30",
+      price: "0.4 ETH",
     },
     {
-      title: "Avocado",
+      title: "Mossy Pillow",
       img: "/images/nft5.jpg",
-      price: "$15.70",
+      price: "0.5 ETH",
     },
     {
-      title: "Lemon 2",
+      title: "Fuzzy Sleepmask",
       img: "/images/nft6.jpg",
-      price: "$8.00",
+      price: "0.1 ETH",
     },
     {
-      title: "Banana",
+      title: "Happy Sleepmask",
       img: "/images/nft7.jpg",
-      price: "$7.50",
+      price: "0.2 ETH",
     },
     {
-      title: "Watermelon",
+      title: "Soft Sleepmask",
       img: "/images/nft8.jpg",
-      price: "$12.20",
+      price: "0.3 ETH",
     },
   ];
 
@@ -63,12 +64,15 @@ export default function Home() {
           </Breadcrumbs>
         </div>
       </section>
-      <section className="relative">
-        <div className="absolute top-0 -z-10 h-full w-full bg-white"><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div></div>
+      <section className="relative overflow-hidden">
+        <div className="absolute top-0 h-full w-full bg-white">
+          <div className="absolute left-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-primary opacity-20 blur-[80px]"></div>
+          <div className="absolute right-0 bottom-0 h-[400px] w-[400px] translate-y-[200px] rounded-full bg-primary opacity-20 blur-[80px]"></div>
+        </div>
         <div className="max-w-6xl h-full py-8 md:py-10 mx-auto relative">
-          <div className="max-w-4xl mb-10">
-            <p className="text-2xl font-medium">Turu NFT Marketplace</p>
-            <p className="text-1xl font-medium mb-4 text-primary">Dapatkan NFT Eksklusif & Mulai Menghasilkan Imbalan dari Tidur Anda</p>
+          <div className="max-w-3xl mb-10">
+            <p className="text-2xl font-medium"><span className="text-primary font-bold">Turu</span> NFT Marketplace</p>
+            <p className="text-1xl mb-4 text-primary">Dapatkan NFT Eksklusif & Mulai Menghasilkan Imbalan dari Tidur Anda</p>
             <p className="text-slate-500">Selamat datang di marketplace resmi Turu! Di sini, Anda bisa membeli NFT yang akan membuka akses ke fitur eksklusif dan imbalan berbasis kualitas tidur. Pilih NFT yang sesuai dengan gaya tidur Anda dan mulai perjalanan menuju tidur yang lebih sehat dan produktif!</p>
           </div>
           <div className="gap-4 grid grid-cols-2 sm:grid-cols-5 w-full">
@@ -85,9 +89,14 @@ export default function Home() {
                     width="100%"
                   />
                 </CardBody>
-                <CardFooter className="text-small justify-between">
-                  <b>{item.title}</b>
-                  <p className="text-default-500">{item.price}</p>
+                <CardFooter className="text-small flex-col">
+                  <div className="flex justify-between mb-2 w-full">
+                    <b>{item.title}</b>
+                    <p className="text-default-500">{item.price}</p>
+                  </div>
+                  <Button color="primary" variant="shadow" size="sm" fullWidth>
+                    Buy NFT
+                  </Button>
                 </CardFooter>
               </Card>
             ))}
